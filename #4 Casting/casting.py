3db0bfb6ee98ec194cd5f8eb755e428aceadb36a
Mirsa Alif Putra <mirsaSoftBoy@gmail.com>
2025-01-18 10:17:02 +0700

    Create casting.py

---
+++ #4 Casting/casting.py
@@ -0,0 +1,44 @@
+# TENTUKAN JENIS VARIABEL
+
+# Mungkin ada saatnya ingin menentukan tipe pada variabel. Ini dapat dilakukan dengan casting. Python adalah bahasa berorientasi objek, dan karenanya menggunakan kelas untuk menentukan tipe data, termasuk tipe primitifnya.
+
+# Oleh karenan itu, casting dala python dilakukan dengan menggunakan konstruktor :
+
+# int() membuat bilangan integer dari literal integer, literal float (dengan menghilangkan semua desimal), atau literal string (dengan syarat mewakili bilangan bulat)
+
+# float() membuat bilangan float dari literal integer, litaral float, atau literal string (dengan syarat string tersebut mewakili float atau integer)
+
+# str() membuat string dari berbagai macam tipe data termasuk string, literal integer, dan literan float
+
+# Contoh integer :
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+print("===========================")
+
+# Contoh float :
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+print("===========================")
+
+# Contoh str :
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
