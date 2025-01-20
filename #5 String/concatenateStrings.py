5a34b4b899a9172d27fda664012a8a7717b192db
Mirsa Alif Putra <mirsaSoftBoy@gmail.com>
2025-01-20 21:44:33 +0700

    Create concatenateStrings.py

---
+++ #5 String/concatenateStrings.py
@@ -0,0 +1,19 @@
+# PENGGABUNGAN STRING
+
+# Untuk menggabungkan, atau mengombinasikan, dua string, anda dapat menggunakan operator +.
+
+# Gabungkan variabel a dengan variabel b ke dalam variabel c :
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+print("===========================")
+
+# Untuk menambahkan spasi di antara keduanya, tambahkan " " :
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
